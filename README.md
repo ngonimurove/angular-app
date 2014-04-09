90f3596b402fafd720049aaa24df74b8a4f049cb
Connor James Leech <connorleech@gmail.com>
2014-04-09 11:37:45 +0200

    basic javascript

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ template-project
 jQuery documentation: jquery.com
 
 Twitter Bootstrap 3: getbootstrap.com
+
+About arrays: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 // alert('hello javascript')
 
 // how to log messages to the console
-console.log('console message')
+// console.log('console message')
 
+// DATA TYPES
+//================
 var myString = 'this is a string';
-var myArray = ['one', "two", [3,2,1], 'last element']
+var myNumber = 5.3
 
-console.log('My variable is: ' + myArray)
+var myArray = ['She said "this"', "this isn't", [["inside"],2,1], 'last element']
+// get last element of an array: myArray[myArray.length - 1]
+// access array within an array: myArray[2][1]
+
+var myObject = {
+	key1: 'this is the value',
+	anotherKey: [1,2,3,4]
+}
+// myObject.anotherKey[3] === 4
+
+// FUNCTIONS
+//================
+var myFunction = function(){
+	console.log('comes from myFunction')
+}
+
+console.log(myFunction())
+
